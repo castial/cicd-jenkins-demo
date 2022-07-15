c431cfa77db576b999a139ea1bbd9922a7437c76
G <nikhilg2@deloitte.com>
2022-07-15 10:31:15 +0530

    first commit cicd-jenkins

---
+++ src/test/java/com/example/jenkins/cicdjenkinsdemo/CicdJenkinsDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.jenkins.cicdjenkinsdemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CicdJenkinsDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
